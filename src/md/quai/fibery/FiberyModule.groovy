d781d27c437297739fab43ebc506de71e3144001
Cipher <itayleybovich@gmail.com>
2023-04-13 18:41:56 +0300

    attempt

--- src/md/quai/fibery/FiberyModule.groovy
+++ src/md/quai/fibery/FiberyModule.groovy
@@ -6,14 +6,8 @@ import com.nu.art.pipeline.workflow.variables.Var_Env
 
 
 public class FiberyModule extends Module {
-    public static final String readyForDevId = "830ad041-f267-11ec-8e36-eb41e3940f9d";
-    public static final String inProgressId = "932ca1ec-cf29-4a73-b062-c917d8cde832";
-    public static final String devDoneId = "939a50c1-f267-11ec-8e36-eb41e3940f9d";
-    public static final String toValidateInStg = "95c0da42-f267-11ec-8e36-eb41e3940f9d";
-    public static final String validatedInStg = "6464ad60-31b3-11ed-a2a4-9520bd30202b";
-    public static final String toValidateInProd = "6f480011-31b3-11ed-a2a4-9520bd30202b";
-
     private String token;
+    public String env;
     def envProjects = [:]
 
     public void declareConfig(String env, FiberyEnvConfig config) {
@@ -28,10 +22,13 @@ public class FiberyModule extends Module {
         this.token = token;
     }
 
+    public void setEnv(String env) {
+        this.env = env;
+    }
+
     public String[] promoteTasks(String[] taskPublicIds) {
-        String env = new Var_Env("BRANCH_NAME").get();
         this.logInfo('############# HERE - promoteTasks #############')
-        FiberyTransaction transaction = new FiberyTransaction(token, "dev");
+        FiberyTransaction transaction = new FiberyTransaction(token, this.env);
         transaction.queryTasks(taskPublicIds);
         return taskPublicIds;
     }
